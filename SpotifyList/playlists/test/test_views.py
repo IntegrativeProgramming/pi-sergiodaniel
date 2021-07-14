8256b1a6bbd576a05ce1f099c50f5c6cfaac2b8b
d.paz1 <d.paz1@udc.es>
2021-07-14 17:11:38 +0200

    test_empezando

--- SpotifyList/playlists/test/test_views.py
+++ SpotifyList/playlists/test/test_views.py
@@ -2,15 +2,15 @@ from django.test import TestCase
 from django.test import Client
 
 
-# from django.contrib.auth import models
-# from django.contrib.auth.models import User
-# from django.contrib.auth import *
-# #from django.utils.importlib import import_module
-# from django.conf import settings
+from django.contrib.auth import models
+from django.contrib.auth.models import User
+from django.contrib.auth import *
+#from django.utils.importlib import import_module
+from django.conf import settings
 
-# from playlists.views import *
+from playlists.views import *
 
-# import requests
+import requests
  
 # class LoginTestCase(TestCase):
         
@@ -36,40 +36,34 @@ from django.test import Client
 
 class TestsUsingClient(TestCase):
 
-    # def test_info_artista(self):
-    #     c = Client()
-    #     response = c.post('playlists/info_artista/', {​​"bohemian rhapsody"}​​​)
-
-    #     self.assertEquals(response.status_code, 200)
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='12test12')
+        self.user.save()
 
     def test_login(self):
         c = Client()
-        response = c.post(
-            'playlists/iniciarsesion/', {'username': 'test', 'klk': '12test12'})
-        self.assertEquals(response.status_code, 200)
+        response = c.login(username='test', password='12test12')
+        self.assertEquals(response, True)
 
+    def test_cerrarsesion(self):
+        c = Client()
+        response = c.get('/playlists/cerrarsesion/')
+        self.assertEquals(response.status_code, 302)
 
+    def test_login_spotify_genius(self):
+        c = Client()
+        response = c.get('/playlists/login_spotify/')
+        self.assertEquals(response.status_code, 302)
 
+        response2 = c.get('/playlists/login_genius/')
+        self.assertEquals(response2.status_code, 302)
 
+    def test_callback(self):
+        c = Client()
+        response = c.get('/playlists/callback/',{'code':'safsadgfdsfagd', 'state':'sjfvhasdasfd'},{'Authorization':'holiii'})
+        self.assertEquals(response.status_code, 302)
 
-
-
-
-# class SpotifyAuthenticationTestCase(TestCase):
- 
-
- 
-#     def test_login_spotify_genius(self):
-#         response = requests.get("http://127.0.0.1:8000/playlists/login_spotify/")
-#         self.assertEquals(response.status_code, 200)
-
-#         response_genius = requests.get("http://127.0.0.1:8000/playlists/login_genius/")
-#         self.assertEquals(response_genius.status_code, 200)
-
-#     def test_cerrarsesion(self):
-#         response = cerrarsesion()
-#         self.assertEquals(response.status_code, 200)
-
-#     def test_infoartista(self):
-#         response = requests.get("http://127.0.0.1:8000/playlists/info_artista/5f7VJjfbwm532GiveGC0ZK/track_name%3DWants%20and%20Needs%20(feat.%20Lil%20Baby)/")
-#         self.assertEquals(response.status_code, 200)
+    # def test_info_artista(self):
+    #     c = Client()
+    #     response = c.get('/playlists/info_artista/', {'track_name': 'hola'})
+    #     self.assertEquals(response.status_code, 200)
