750b0d72897dfd7286144c69795bb4a92726a321
d.paz1 <d.paz1@udc.es>
2021-07-15 18:26:43 +0200

    test_siguiendo

--- SpotifyList/playlists/test/test_views.py
+++ SpotifyList/playlists/test/test_views.py
@@ -32,7 +32,6 @@ import requests
 #     def test_login_wrong_pssword(self):
 #         user = authenticate(username='test', password='wrong')
 #         self.assertFalse(user is not None and user.is_authenticated)
- 
 
 class TestsUsingClient(TestCase):
 
@@ -54,19 +53,65 @@ class TestsUsingClient(TestCase):
         c = Client()
         response = c.get('/playlists/login_spotify/')
         self.assertEquals(response.status_code, 302)
-
         response2 = c.get('/playlists/login_genius/')
         self.assertEquals(response2.status_code, 302)
 
-    # def test_home(self): FALLO access_token KEY_ERROR
-    #     c = Client()
-    #     response = c.get('/playlists/home/',{'access_token':'safjbsakd'})
-    #     self.assertEquals(response.status_code, 200)        
+    def test_grafico_canciones(self):
+        c = Client()
+        response_get = c.get('/playlists/grafico_canciones/')
+        self.assertEquals(response_get.status_code, 200)
+
+    def test_mostrar_playlists(self):
+        c = Client()
+        response = c.get('/playlists/mostrar_playlists/')
+        self.assertEquals(response.status_code, 302)
 
     # def test_callback(self): FALLO HEADERS , FALTA UNO
     #     c = Client()
     #     response = c.get('/playlists/callback/',{'code':'safsadgfdsfagd', 'state':'sjfvhasdasfd'},{'header':'Basic kljxzbvds'})
     #     self.assertEquals(response.status_code, 302)
+
+    # callback_genius
+
+    # def test_home(self):
+    #     c = Client()
+    #     response = c.get('/playlists/home/', {'Authorization': preparedString})
+    #     self.assertEquals(response.status_code, 200)   
+
+    # def test_mostrar_tracks(self): 404
+    #     c = Client()
+    #     response = c.post('/playlists/mostrar_tracks/',{'track_name':'hola', 'nombre_playlist':'Rap Caviar' 
+    #         ,'playlist_id':'47RwPX4akLGe6OqyGpcWMd'})
+    #     self.assertEquals(response.status_code, 302)
+
+    def test_add_playlist(self):
+        client_id = '2e6a6b883a174b3693a4c0a335558f30'
+        client_secret = 'a2fbd32563c04123a3515c45951206ca'
+        auxString = '{}:{}'.format(client_id, client_secret)
+        preparedString = base64.b64encode(auxString.encode()).decode()
+
+        c = Client()
+
+        response_get = c.get('/playlists/add_playlist/')
+        self.assertEquals(response_get.status_code, 200)
+        # response = c.post('/playlists/add_playlist/',
+        #     {'playlist_name':'playlistTEST','playlist_description':'wakanda', 
+        #     'playlist_type':'public'}, {'Authorization': preparedString},
+        #     HTTP_ACCEPT='application/json')
+        # self.assertEquals(response.status_code, 302)
+
+    # def test_add_searched_playlist(self):
+    #     c = Client()
+    #     response_get = c.get('/playlists/add_searched_playlist/', 
+    #         {'playlist_id':'2DiQ9JVYVSrJZVETtTLRpb', 'nombre_playlist':'ADISTA'})
+    #     self.assertEquals(response_get.status_code, 200)
+
+
+    # def test_playlist_detail(self):
+    #     c = Client()
+    #     response = c.get('/playlists/playlist_detail/2DiQ9JVYVSrJZVETtTLRpb/ADISTA/' )
+    #     self.assertEquals(response.status_code, 302)
+
 
     # def test_info_artista(self):
     #     c = Client()
