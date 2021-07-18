581a61ffdaac9a920ef1a456d6407f1cb89ed203
d.paz1 <d.paz1@udc.es>
2021-07-18 16:17:17 +0200

    finalizando

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pi-sergiosdaniel
+
+- Para ejecutar lanzar el script setup.sh , se puede hacer con compose-up o con docker pull, las opciones aparecen comentadas en el propio script.
+
+- Respecto a fallos conocidos y otros aspectos importantes hemos actualizado el doc de la propuesta, ahí está todo incluído.
+
+- Las horas de trabajo se han especificado al final del documento citado anteriormente.
--- SpotifyList/setup.sh
+++ SpotifyList/setup.sh
@@ -1,4 +1,9 @@
 
-sudo docker build .
-sudo docker-compose down
-sudo docker-compose up --force-recreate
+#!/bin/bash
+# En caso de hacerlo con compose-up.
+docker compose-up # Desde carpeta dockerfile y ya se levanta en windows.
+
+# En caso de tener la imagen subida al Docker Hub descomentar y comentar arriba.
+# docker pull sergiopardofernandez/spotifylist_web:v1
+# docker run -it sergiopardofernandez/spotifylist_web:v1
+
