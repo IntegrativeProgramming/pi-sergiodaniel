05c9f5b4c7b82277120d3fe2bc252fd863b33f4f
d.paz1 <d.paz1@udc.es>
2021-07-18 16:55:02 +0200

    last_commit

--- SpotifyList/setup.sh
+++ SpotifyList/setup.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
+
 # En caso de hacerlo con compose-up.
-docker compose-up # Desde carpeta dockerfile y ya se levanta en windows.
+#docker compose-up # Desde carpeta donde esta el dockercompose.yaml
 
-# En caso de tener la imagen subida al Docker Hub descomentar y comentar arriba.
-# docker pull sergiopardofernandez/spotifylist_web:v1
-# docker run -it sergiopardofernandez/spotifylist_web:v1
-
+# En caso de tener la imagen subida al Docker Hub descomentar y comentar arriba, como es nuestro caso.
+docker pull sergiopardofernandez/spotifylist_web:v1
+docker run -p 8000:8000 -v /code  sergiopardofernandez/spotifylist_web:v1 python3 manage.py runserver 0.0.0.0:8000
